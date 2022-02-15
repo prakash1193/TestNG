8eb1a61f2b315637391bbda116cccb2bbadd8cb8
pec <pec@DESKTOP-JBDBHD6>
2022-02-15 19:25:18 +0530

    second commit

---
+++ src/testNG_Check/ExecutionClass.java
@@ -0,0 +1,26 @@
+package testNG_Check;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class ExecutionClass {
+
+	@BeforeClass
+	public void beforeClass() 
+	{
+		System.out.println("before class");
+		System.out.println("before class");
+	}
+	@BeforeMethod 
+	public void beforeMethod() 
+	{
+		System.out.println("before method");
+	}
+	
+	@Test
+	public void test1() 
+	{
+		System.out.println("test1");
+	}
+}
